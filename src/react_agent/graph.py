7fb348bc1838a26269cc07726026e449d5a66959
leovs09 <leovs010@gmail.com>
2025-03-02 05:41:22 +0100

    preload memory during interaction

--- src/react_agent/graph.py
+++ src/react_agent/graph.py
@@ -1,7 +1,8 @@
-# Import core components 
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 from langgraph.store.memory import InMemoryStore
 from langmem import create_manage_memory_tool, create_search_memory_tool
+from react_agent import prompts
 
 # Set up storage 
 store = InMemoryStore(
@@ -11,13 +12,20 @@ store = InMemoryStore(
     }
 ) 
 
+checkpointer = MemorySaver() # Checkpoint graph state
+
 # Create an agent with memory capabilities 
 graph = create_react_agent(
     "anthropic:claude-3-5-sonnet-latest",
+    prompt=prompts.prompt,
     tools=[
-        # Memory tools use LangGraph's BaseStore for persistence (4)
+        # Add memory tools 
+        # The agent can call "manage_memory" to
+        # create, update, and delete memories by ID
+        # Namespaces add scope to memories. To
+        # scope memories per-user, do ("memories", "{user_id}"):
         create_manage_memory_tool(namespace=("memories",)),
-        create_search_memory_tool(namespace=("memories",)),
     ],
     store=store,
+    checkpointer=checkpointer,
 )
--- src/react_agent/prompts.py
+++ src/react_agent/prompts.py
@@ -1,5 +1,27 @@
+from langgraph.utils.config import get_store 
+
 """Default prompts used by the agent."""
 
 SYSTEM_PROMPT = """You are a helpful AI assistant.
 
 System time: {system_time}"""
+
+async def prompt(state):
+    """Prepare the messages for the LLM."""
+    # Get store from configured contextvar; 
+    store = get_store() # Same as that provided to `create_react_agent`
+    memories = await store.asearch(
+        # Search within the same namespace as the one
+        # we've configured for the agent
+        ("memories",),
+        query=state["messages"][-1].content,
+    )
+    system_msg = f"""You are a helpful assistant.
+
+## Memories
+<memories>
+{memories}
+</memories>
+"""
+    return [{"role": "system", "content": system_msg}, *state["messages"]]
+
