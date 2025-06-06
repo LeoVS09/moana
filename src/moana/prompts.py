65770b1d372cf575a0dbc216c5178e5fec9e5e3b
leovs09 <leovs010@gmail.com>
2025-06-06 22:51:36 +0200

    remove langgraph

--- src/moana/prompts.py
+++
@@ -1,7 +0,0 @@
-from langgraph.utils.config import get_store 
-
-"""Default prompts used by the agent."""
-
-SYSTEM_PROMPT = """You are a helpful and friendly AI assistant. Get to know the user!"""
-
-
--- tests/unit_tests/test_configuration.py
+++
@@ -1,5 +0,0 @@
-from moana.configuration import Configuration
-
-
-def test_configuration_empty() -> None:
-    Configuration.from_runnable_config({})
